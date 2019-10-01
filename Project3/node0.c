2a58cf01654c14fcbcdaf4927b29022af783e0b2
nwalzer <nswalzer@gmail.com>
2019-10-01 14:06:23 -0400

    Starting up

--- Project3/node0.c
+++ Project3/node0.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "project3.h"
+#include <string.h>
 
 extern int TraceLevel;
 
@@ -11,8 +12,46 @@ struct NeighborCosts   *neighbor0;
 
 /* students to write the following two routines, and maybe some others */
 
-void rtinit0() {
+//printdt0(int MyNodeNumber, struct NeighborCosts *neighbor, struct distance_table *dtptr);
 
+int neighborIDs[MAX_NODES];
+
+int THISNODE = 0;
+
+void rtinit0() {
+	neighbor0 = getNeighborCosts(THISNODE);
+	int totalNodes = neighbor0->NodesInNetwork;
+	int i = 0;
+	int j = 0;
+
+	for(i = 0; i < MAX_NODES; i++){ //set everything in the costs array to infinite distance
+		neighborIDs[i] = -1; //initialize neighbor IDs to invalid value
+		for(j = 0; j < MAX_NODES; j++){
+			dt0.costs[i][j] = INFINITY;
+		}
+	}
+
+	j = 0;
+	for(i = 0; i < totalNodes; i++){ //establish this node's distance to each other node (1 hop)
+		dt0.costs[THISNODE][i] = neighbor0->NodeCosts[i];
+		if(neighbor0->NodeCosts[i] != INFINITY){ //if we have a direct connection to this node
+			neighborIDs[j] = i;
+			j++;
+		}
+	}
+	printdt0(0, neighbor0, &dt0);
+	i = 0;
+	while(neighborIDs[i] != -1){
+		if(i == THISNODE){ //don't send to self
+			continue;
+		}
+		struct RoutePacket toSend;
+		toSend.sourceid = 0;
+		toSend.destid = i;
+		memcpy(&toSend.mincost, &dt0.costs[THISNODE], MAX_NODES * sizeof(int));
+		toLayer2(toSend);
+		i++;
+	}
 }
 
 
